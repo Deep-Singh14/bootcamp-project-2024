7d6a4662e365fdcf6a8fcd9ce80aad35079592b2
Deep-Singh14 <khushdip14@gmail.com>
2024-10-19 20:06:39 -0700

    feat: created list of blog objects

---
+++ src/blog.ts
@@ -0,0 +1,27 @@
+type Blog = {
+    title: string
+    date: string
+    description: string
+    image: string
+    imageAlt: string
+    slug: string
+}; 
+
+const blogs: Blog[] = [
+    {
+        title: "Downtown",
+        date: "10/16/2024",
+        description: "Went downtown to eat some Thai food",
+        image: "",
+        imageAlt: "",
+        slug: ""
+    },
+    {
+        title: "Milestone 1",
+        date: "10/19/2024",
+        description: "First milestone in H4I bootcamp",
+        image: "",
+        imageAlt: "",
+        slug: ""
+    }
+]
