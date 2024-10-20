081aaf9ba4e20dd8ec839715cd9657c214636f42
Deep-Singh14 <khushdip14@gmail.com>
2024-10-19 22:08:39 -0700

    file: added html for each blog page

--- src/blog.ts
+++ src/blog.ts
@@ -14,7 +14,7 @@ const blogs: Blog[] = [
         description: "Went downtown to eat some Thai food",
         image: "",
         imageAlt: "",
-        slug: ""
+        slug: "downtown"
     },
     {
         title: "Milestone 1",
@@ -22,7 +22,7 @@ const blogs: Blog[] = [
         description: "First milestone in H4I bootcamp",
         image: "",
         imageAlt: "",
-        slug: ""
+        slug: "milestone1"
     }
 ]
 
@@ -30,14 +30,27 @@ const blogs: Blog[] = [
 const blogContainer = document.getElementById("blog-container")
 
 blogs.forEach(blog => {
+    //Create different elements
     const blogDiv = document.createElement("div")
     const blogTitle = document.createElement("h1")
     const blogImage = document.createElement("img")
     const blogDescription = document.createElement("p")
+    const blogHREF = document.createElement("a")
+
+    //Add text and such to each element
+    blogTitle.textContent = blog.title;
+    blogImage.src = blog.image;
+    blogImage.alt = blog.imageAlt;
+    blogDescription.textContent = blog.description;
+
+    //Adds the HREEF to the blogHREF element:  <a href = "...">MORE</a>
+    blogHREF.href = `blogs/${blog.slug}.html`
+    blogHREF.textContent = "MORE";
 
     blogDiv.appendChild(blogTitle)
     blogDiv.appendChild(blogImage)
     blogDiv.appendChild(blogDescription)
+    blogDiv.appendChild(blogHREF)
 
     blogContainer?.appendChild(blogDiv) //blogContainer possibly null -> added '?'
 })
