ed9977079e9e93a0f4322589bb2f6a96b6cff81e
Deep-Singh14 <khushdip14@gmail.com>
2024-10-23 14:05:36 -0700

    feat: fixed blogs not showing up

--- src/blog.js
+++ src/blog.js
@@ -5,7 +5,7 @@ var blogs = [
         description: "Went downtown to eat some Thai food",
         image: "",
         imageAlt: "",
-        slug: ""
+        slug: "downtown"
     },
     {
         title: "Milestone 1",
@@ -13,17 +13,28 @@ var blogs = [
         description: "First milestone in H4I bootcamp",
         image: "",
         imageAlt: "",
-        slug: ""
+        slug: "milestone1"
     }
 ];
 var blogContainer = document.getElementById("blog-container");
 blogs.forEach(function (blog) {
+    //Create different elements
     var blogDiv = document.createElement("div");
     var blogTitle = document.createElement("h1");
     var blogImage = document.createElement("img");
     var blogDescription = document.createElement("p");
+    var blogHREF = document.createElement("a");
+    //Add text and such to each element
+    blogTitle.textContent = blog.title;
+    blogImage.src = blog.image;
+    blogImage.alt = blog.imageAlt;
+    blogDescription.textContent = blog.description;
+    //Adds the HREEF to the blogHREF element:  <a href = "...">MORE</a>
+    blogHREF.href = "../bootcamp-project-2024/src/blogs/".concat(blog.slug, ".html");
+    blogHREF.textContent = "MORE";
     blogDiv.appendChild(blogTitle);
     blogDiv.appendChild(blogImage);
     blogDiv.appendChild(blogDescription);
+    blogDiv.appendChild(blogHREF);
     blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(blogDiv); //blogContainer possibly null -> added '?'
 });
