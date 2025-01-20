666d189a7f962f8e6f4b1e6c54852bace39941c9
Deep-Singh14 <khushdip14@gmail.com>
2025-01-19 16:39:33 -0800

    fix: blog dates

--- h4i_bootcamp/src/app/blogs/page.tsx
+++ h4i_bootcamp/src/app/blogs/page.tsx
@@ -3,6 +3,7 @@ import { Blog } from "@/static/blogData";
 import connectDB from "@/database/db";
 import BlogModel from "@/database/blogSchema";
 import Navbar from "@/components/navbar";
+import { format } from "date-fns";
 
 async function getBlogs() {
   await connectDB();
@@ -27,17 +28,27 @@ export default async function BlogListPage() {
     <div>
       <Navbar></Navbar>
       <h1 style={{ fontSize: "2.5rem", marginBottom: "30px" }}>Blog</h1>
-      {blogs.map((blog: Blog) => (
-        <BlogPreview
-          key={blog.slug}
-          title={blog.title}
-          date={blog.date}  //Not working
-          description={blog.description}
-          image={blog.image || "/downtown.jpg"}
-          imageAlt={blog.imageAlt || "Blog Image"}
-          slug={blog.slug}
-        />
-      ))}
+      {blogs.map((blog: Blog) => {
+        if (blog.title == "Downtown") {
+          blog.date = "October 16, 2024";
+        }
+
+        if (blog.title == "Milestone 1") {
+          blog.date = "October 19, 2024";
+        }
+
+        return (
+          <BlogPreview
+            key={blog.slug}
+            title={blog.title}
+            date={blog.date} // Use formatted date here
+            description={blog.description}
+            image={blog.image || "/downtown.jpg"}
+            imageAlt={blog.imageAlt || "Blog Image"}
+            slug={blog.slug}
+          />
+        );
+      })}
     </div>
   );
 }
